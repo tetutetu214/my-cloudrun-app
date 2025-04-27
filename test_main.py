cf051e0d90494511a3c06edb02af7d3971513577
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2025-04-27 06:22:34 +0000

    Fix test_main.py

--- test_main.py
+++ test_main.py
@@ -1,6 +1,12 @@
-# test_main.py
+# テスト対象のファイルをインポート
 import main
 
 def test_hello_http():
+    """
+    hello_http関数が'Hello World!!!'を返すことを確認するテスト。
+    このテストはAPIのグリーティングメッセージが正しいことを検証します。
+    """
+    # 引数をNone(無し)で hello_http関数を実際に呼び出し
     response = main.hello_http(None)
+    # 関数の戻り値が"Hello World!!!"と等しい
     assert response == "Hello World!!!"
